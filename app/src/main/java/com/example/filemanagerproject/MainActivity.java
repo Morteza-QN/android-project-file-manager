0d5b4012ec4ca9f68211d1063a3b6f2231eea5cc
morteza <mortezaghorbani3@gmail.com>
2020-05-05 17:35:48 +0430

    add dialog ui

--- app/src/main/java/com/example/filemanagerproject/MainActivity.java
+++ app/src/main/java/com/example/filemanagerproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.filemanagerproject;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_main_fragmentContainer, new FileListFragment());
+        transaction.commit();
     }
 }
